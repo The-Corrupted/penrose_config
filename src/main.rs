c6bd0018269397c7ff03f5e03152ce08c80f449a
The-Corrupted <intothedark.me@gmail.com>
2021-12-27 14:47:06 -0800

    Change to use x11rb backend

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// TODO: Add X11rb backed status bar
+
 #[macro_use]
 extern crate penrose;
 
@@ -5,31 +7,27 @@ use penrose::{
     contrib::{
         actions::{update_monitors_via_xrandr},
         extensions::{Scratchpad},
-        hooks::{DefaultWorkspace, LayoutSymbolAsRootName, RemoveEmptyWorkspaces},
+        hooks::{LayoutSymbolAsRootName, RemoveEmptyWorkspaces},
         layouts::paper,
     },
     core::{
         config::Config,
         helpers::index_selectors,
-        hooks::Hooks,
         layout::{bottom_stack, side_stack, Layout, LayoutConf},
         data_types::RelativePosition,
     },
     logging_error_handler,
-    xcb::new_xcb_backed_window_manager,
-    xcb::new_xcb_backed_status_bar,
+    x11rb::new_x11rb_rust_backed_window_manager,
+    x11rb::X11rbHooks,
     Backward, Forward, Less, More, Selector,
-    draw::{Color, TextStyle}, 
 };
 
 use simplelog::{LevelFilter, SimpleLogger};
 
 const TERM: &str = "alacritty";
-const BROWSER: &str = "firefox";
 const SHOW_RUN: &str = "rofi -show run";
 const MON_1: &str = "DP-2";
 const MON_2: &str = "DP-4";
-const BAR_HEIGHT: u32 = 30; 
 
 fn my_layouts() -> Vec<Layout> {
     let n_main = 1;
@@ -50,36 +48,20 @@ fn my_layouts() -> Vec<Layout> {
 
 
 fn main() -> penrose::Result<()> {
-    SimpleLogger::init(LevelFilter::Debug, simplelog::Config::default())
-        .expect("failed to init logging");
+    // SimpleLogger::init(LevelFilter::Debug, simplelog::Config::default()).expect("failed to init logging");
 
     let mut config_builder = Config::default().builder();
     let config = config_builder
         .workspaces(vec!["[1]", "[2]", "[3]", "[4]"])
         .layouts(my_layouts())
-        .bar_height(BAR_HEIGHT)
-        .top_bar(true)
         .build()
         .unwrap();
 
     let sp = Scratchpad::new(TERM, 0.8, 0.8);
 
-    let sb = new_xcb_backed_status_bar(BAR_HEIGHT as usize, &TextStyle {
-        font: "Hack".to_string(), 
-        point_size: 12, 
-        fg: Color::new_from_hex(0xffffff), 
-        bg: Some(Color::new_from_hex(0x000000)), 
-        padding: (2.0, 2.0), 
-    }, Color::new_from_hex(0x123456), Color::new_from_hex(0x654321), config.workspaces().clone())?;
-
-    let hooks: Hooks<_> = vec![
+    let hooks: X11rbHooks = vec![
         LayoutSymbolAsRootName::new(),
         RemoveEmptyWorkspaces::new(config.workspaces().clone()),
-        DefaultWorkspace::new("1term", "[side]", vec![TERM]),
-        DefaultWorkspace::new("2term", "[botm]", vec![TERM, TERM]),
-        DefaultWorkspace::new("3term", "[side]", vec![TERM, TERM, TERM]),
-        DefaultWorkspace::new("web", "[papr]", vec![BROWSER]),
-        Box::new(sb),
         sp.get_hook(),
     ];
 
@@ -127,16 +109,8 @@ fn main() -> penrose::Result<()> {
         };
     };
     
-    let sb = new_xcb_backed_status_bar(BAR_HEIGHT as usize, &TextStyle {
-        font: "Hack".to_string(), 
-        point_size: 12, 
-        fg: Color::new_from_hex(0xffffff), 
-        bg: Some(Color::new_from_hex(0x000000)), 
-        padding: (2.0, 2.0), 
-    }, Color::new_from_hex(0x123456), Color::new_from_hex(0x654321), config.workspaces().clone())?;
 
-
-    let mut wm = new_xcb_backed_window_manager(config, hooks, logging_error_handler())?;
+    let mut wm = new_x11rb_rust_backed_window_manager(config, hooks, logging_error_handler())?;
     update_monitors_via_xrandr(MON_2, MON_1, RelativePosition::Left)?;
     wm.grab_keys_and_run(key_bindings, map!{})
 }
